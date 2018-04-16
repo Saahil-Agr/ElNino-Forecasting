2295c5821b8362d815ddfd366d58d2e67e3f6124
Brent Lunghino <brentlunghino@gmail.com>
2018-04-16 17:39:19 -0400

    fixed bugs in writing images portion of script

--- preprocess_gcm.py
+++ preprocess_gcm.py
@@ -85,13 +85,20 @@ def main(data_dir='.', preprocess_netcdfs=False, write_images=True,
         open preprocessed netcdf files containing regridded anomaly data.
         write each timestep to an individual image file 
     """
-    ## SETTINGS
+    ##############
+    ## SETTINGS ##
+    ##############
+    ## path to the .nc file whose grid we will interpolate onto 
     grid_nc = 'GCMs/MPI-ESM-LR/piControl/tas_Amon_MPI-ESM-LR_piControl_r1i1p1_185001-203512.nc'
+    ## these are the GCMs to preprocess
     gcm_names = ['CNRM-CM5']#, 'CanESM2', 'HadGEM2-ES', 'MPI-ESM-LR',]
+    ## these are the experiments and forcings settings for each gcm
     scenarios = {
         'CNRM-CM5': ['piControl_r1i1p1'],
     }
-    channels = ['tas', 'psl']
+    ## these are the variables to work with
+    channels = ['tas']
+    ## interpolate onto common grid and calculate anomalies
     if preprocess_netcdfs:
         with xr.open_dataset(join(data_dir, grid_nc)) as grid_ds:
             ## interpolate gcm onto this grid
@@ -101,10 +108,11 @@ def main(data_dir='.', preprocess_netcdfs=False, write_images=True,
             rg_fstr = '{}_Amon_{}_{}_{}_{}_%sx%sregrid_anomalies.nc' % (str(X.shape[0]), str(X.shape[1]))
             for gcm_name in gcm_names:
                 for scenario in scenarios[gcm_name]:
+                    ## path for gcm files
                     gcm_path = join(data_dir, 'GCMs', gcm_name, scenario, '*.nc')
                     with xr.open_mfdataset(gcm_path) as gcm:
                         gcm_grid = (np.asarray(gcm.lat), np.asarray(gcm.lon))
-                        # gcm to 365 day calendar
+                        # force gcm to 365 day calendar
                         date_strings = [str(d)[:10] for d in gcm.time.values]
                         try:
                             dr = pd.to_datetime(date_strings)
@@ -114,17 +122,20 @@ def main(data_dir='.', preprocess_netcdfs=False, write_images=True,
                         ## loop over years
                         year_range = pd.date_range(dr[0], dr[-1], frequency='100Y')
                         for var in channels:
-                            ## compute anomalies by removing seasonal mean for dataset
+                            ## compute anomalies by removing mean for each location for each month
                             for m in range(1, 13):
                                 filtr = dr.month == m
                                 ss = gcm[var][filtr,:,:].mean()
                                 gcm[var][filtr,:,:] = gcm[var][filtr,:,:] - ss
+                            ## iterate in hundred year chunks to keep new .nc file sizes under control
                             for y0, y1 in zip(year_range[:-1], year_range[1:]):
                                 print('Interpolating', var, y0, y1)
                                 filtr = (dr.year >= y0) and (dr.year < y1)
-                                dt = np.sum(filtr)
+                                ## number of timesteps
+                                n_t = np.sum(filtr)
+                                ## output data will be stored here
                                 sub_gcm = xr.DataArray(
-                                    np.zeros((dt, ncar.lat.shape[0], ncar.lon.shape[0])),
+                                    np.zeros((n_t, ncar.lat.shape[0], ncar.lon.shape[0])),
                                     coords=[
                                         ('time', gcm.time[filtr]), 
                                         ('lat', ncar.lat),
@@ -132,62 +143,74 @@ def main(data_dir='.', preprocess_netcdfs=False, write_images=True,
                                     ]
                                 )
                                 sub_gcm.attrs = gcm[var].attrs
-                                ## loop over days in that year
                                 i = np.min(np.where(filtr)[0])
-                                for j in range(dt):
+                                ## subloop over each month in the 100 year interval
+                                for j in range(n_t):
                                     ## interpolate gcm to ncar grid
                                     f_interp = RegularGridInterpolator(
                                         gcm_grid, 
                                         np.asarray(gcm[var][i + j,:,:]),
-                                        bounds_error=False,
-                                        fill_value=None,
+                                        # bounds_error=False,
+                                        # fill_value=None,
                                     )
                                     interp_data = f_interp(xi, method='linear')
                                     sub_gcm[j,:,:] = interp_data.reshape(Y.shape).T
                                 sub_gcm = sub_gcm.to_dataset(name=var)
                                 sub_gcm.attrs = gcm.attrs
+                                ## path to write out file
                                 fp = join(gcm_path, rg_fstr.format(var, gcm_name, scenario, y0, y1))
+                                ## save 100 years of regridded anomalies data as .nc file
                                 sub_gcm.to_netcdf(fp, mode='w', unlimited_dims=['time'])
         print('Finished Interpolating')
     if generate_target:
         pass
     if write_images:
-        print('Writing image files')
+        print('Writing {} files'.format(img_ext))
         channels_str = (len(channels) * '{}-').format(*channels)[:-1]
-        img_fstr = '%s_Amon_{}_{}_{}-{}.%s' % (channels_str, img_ext)
+        img_fstr = '%s_Amon_{}_{}_{}.%s' % (channels_str, img_ext)
+        ## number of bits depending on datatype
         if img_type is np.uint32:
             bits = 2 ** 32 - 1
         elif img_type is np.uint8:
             bits = 2 ** 8 - 1
+        ## loop over GCMs and scenarios
         for gcm_name in gcm_names:
             for scenario in scenarios[gcm_name]:
-                gcm_path = join(data_dir, 'GCMs', gcm_name, scenario, '*.nc')
-# gcm_path = join(data_dir, 'GCMs', gcm_name, scenario, '*regrid_anomalies.nc')
+                print(gcm_name, scenario)
+                # gcm_path = join(data_dir, 'GCMs', gcm_name, scenario, '*.nc')
+                gcm_path = join(data_dir, 'GCMs', gcm_name, scenario, '*regrid_anomalies.nc')
                 with xr.open_mfdataset(gcm_path) as gcm:
-                    for var in channels:
-                        vmin = gcm[var].min()
-                        vmax = gcm[var].max()
-                        ## normalize entire array to 0-1 and scale to image bits
-                        gcm[var] = scale * (gcm[var] - vmin) / vmax
+                    ## if using an integer type, scale to full range of integer
+                    if img_type in (np.uint8, np.unint32):
+                        print('Scaling channels')
+                        for var in channels:
+                            print('  {}'.format(var))
+                            vmin = gcm[var].min()
+                            vmax = gcm[var].max()
+                            ## normalize entire array to 0-1 and scale to image bits (ie 0-255 for 8 bit)
+                            gcm[var] = bits * (gcm[var] - vmin) / vmax
                     ## write image file at each timestep
+                    print('generating images for each timestep')
                     for t in gcm.time:
-                        arr = np.asarray(
-                            [gcm[var].sel(time=t) for var in channels],
-                            dtype=img_type
-                        )
-                        name = img_fstr.format(gcm_name, scenario, t.year, t.month)
-                        path = join(data_dir, 'img', gcm, scenario, name)
+                        print('  {}'.format(t.values))
+                        ## for each channel load data for timestep t into memory and flip array upright
+                        vals  = [np.flipud(np.asarray(gcm[var].sel(time=t))) for var in channels]
+                        ## stack channels together and specify data type, shape = (rows, cols, channels)
+                        arr = np.stack(vals, axis=-1).astype(img_type)
+                        name = img_fstr.format(gcm_name, scenario, str(t.values)[:7])
+                        path = join(data_dir, 'img', gcm_name, scenario, name)
+                        ## .npy file format can take arbitrary color bands and pixel bits
                         if img_ext == 'npy':
-                            np.save(arr, path)
-                        elif image_ext == 'jpg':
-                            # Image.fromarray(arr).save(path)
-                            pass
-
-
-
+                            np.save(path, arr)
+                        ## .jpg with pillow can take 1, 3, or 4 color bands and 8 bits
+                        elif img_ext == 'jpg' and len(channels) == 3:
+                            Image.fromarray(arr, mode='RGB').save(path)
+                        elif img_ext == 'jpg' and len(channels) == 1:
+                            Image.fromarray(arr, mode='L').save(path)
+        print('Finished writing {} files'.format(img_ext))
 
 if __name__ == '__main__':
-    import warnings
-    warnings.filterwarnings('error')
-    main(data_dir='.')
+    # import warnings
+    # warnings.filterwarnings('error')
+    main(data_dir='.', img_ext='npy', img_type=np.float32)
 
