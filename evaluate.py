45f3e9fd9bad5b02e2ed6813e4a11f382c8ea3ac
matiasct <matiasct@stanford.edu>
2018-04-28 22:31:36 -0700

    files with crnn model for training and evaluating, not complete yet

---
+++ evaluate.py
@@ -0,0 +1,88 @@
+
+import logging
+import os
+
+import numpy as np
+import torch
+from torch.autograd import Variable
+import utils
+import model.net as net
+import model.data_loader as data_loader
+
+def evaluate(model, loss_fn, dataloader, metrics, params):
+
+    # set model to evaluation mode
+    model.eval()
+
+    # summary for current eval loop
+    eval_losses = []
+
+    # compute metrics over the dataset
+    for data_batch, labels_batch in dataloader:
+
+        # move to GPU if available
+        if torch.cuda.is_available():
+            data_batch, labels_batch = data_batch.cuda(async=True), labels_batch.cuda(async=True)
+
+        # fetch the next evaluation batch
+        data_batch, labels_batch = Variable(data_batch), Variable(labels_batch)
+
+        # compute model output
+        output_batch = model(data_batch)
+        loss = loss_fn(output_batch, labels_batch)
+
+        # extract data from torch Variable, move to cpu, convert to numpy arrays
+        output_batch = output_batch.data.cpu().numpy()
+        labels_batch = labels_batch.data.cpu().numpy()
+
+        eval_losses.append(loss.data[0])
+        loss_avg = torch.mean(torch.FloatTensor(eval_losses))
+        logging.info("- Eval average loss : " + loss_avg)
+
+    return metrics_mean
+
+
+if __name__ == '__main__':
+    """
+        Evaluate the model on the test set.
+    """
+    # Load the parameters
+    args = parser.parse_args()
+    json_path = os.path.join(args.model_dir, 'params.json')
+    assert os.path.isfile(json_path), "No json configuration file found at {}".format(json_path)
+    params = utils.Params(json_path)
+
+    # use GPU if available
+    params.cuda = torch.cuda.is_available()     # use GPU is available
+
+    # Set the random seed for reproducible experiments
+    torch.manual_seed(230)
+    if params.cuda: torch.cuda.manual_seed(230)
+
+    # Get the logger
+    utils.set_logger(os.path.join(args.model_dir, 'evaluate.log'))
+
+    # Create the input data pipeline
+    logging.info("Creating the dataset...")
+
+    # fetch dataloaders
+    dataloaders = data_loader.fetch_dataloader(['test'], args.data_dir, params)
+    test_dl = dataloaders['test']
+
+    logging.info("- done.")
+
+    # Define the model
+    model = net.Net(params).cuda() if params.cuda else net.Net(params)
+
+    loss_fn = net.loss_fn
+    metrics = net.metrics
+
+    logging.info("Starting evaluation")
+
+    # Reload weights from the saved file
+    utils.load_checkpoint(os.path.join(args.model_dir, args.restore_file + '.pth.tar'), model)
+
+    # Evaluate
+    test_metrics = evaluate(model, loss_fn, test_dl, metrics, params)
+    save_path = os.path.join(args.model_dir, "metrics_test_{}.json".format(args.restore_file))
+    utils.save_dict_to_json(test_metrics, save_path)
