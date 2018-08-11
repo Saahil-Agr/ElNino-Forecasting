317459a7c96a6282034d87ca33db8fae7273308a
matiasct <matiasct@stanford.edu>
2018-08-10 17:22:08 -0700

    Included the changes of crnn.py and dataloader

--- model/crnn.py
+++ model/crnn.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import init
+import torchvision.models as models
 
 '''
 CNN and CRNN classes, loss and metrics.
@@ -20,109 +21,269 @@ def loss_fn(prediction, target):
 
 class CNN(nn.Module):
 
-    def __init__(self, channels):
+    def __init__(self, variables, channels, vector_dim, dropout=None):
 
         # convolution layers arguments: input_channels, output_channels, filter_size, stride, padding.
         super(CNN, self).__init__()
 
-        self.conv1 = nn.Conv2d(1, channels, 3, stride=1, padding=1)    # output: (batch,96,192,32)
-        self.conv1_bn = nn.BatchNorm2d(channels)
-        self.conv2 = nn.Conv2d(channels, channels*2, 4, 2, 1)  #  (batch,48,96,64)
-        self.conv2_bn = nn.BatchNorm2d(channels*2)
-        self.conv3 = nn.Conv2d(channels*2, channels*4, 4, 2, 1) # (batch,24,48,128)
-        self.conv3_bn = nn.BatchNorm2d(channels*4)
-        self.conv4 = nn.Conv2d(channels*4, channels*8, 4, 2, 1) # (batch,12,24,256)
-        self.conv4_bn = nn.BatchNorm2d(channels*8)
-        self.conv5 = nn.Conv2d(channels*8, channels*16, 4, 2, 1) # (batch,6,12,512)
-        self.conv5_bn = nn.BatchNorm2d(channels*16)
-        self.conv6 = nn.Conv2d(channels*16, channels*16, 4, 1, 0) # (batch,3,6,1024)
-        self.fc = nn.Linear(channels*16*3*9, 1)
-
-    def forward(self, input):
-        x = F.relu(self.conv1_bn(self.conv1(input)))
-        x = F.relu(self.conv2_bn(self.conv2(x)))
-        x = F.relu(self.conv3_bn(self.conv3(x)))
-        x = F.relu(self.conv4_bn(self.conv4(x)))
-        x = F.relu(self.conv5_bn(self.conv5(x)))
-        x = F.relu(self.conv6(x))
-
-        # resize tensor to fit the FC
-        x = x.view(x.size()[0], -1)
-        x = self.fc(x)
-
-        return x.view(-1)
-
-
-
-class CRNN(nn.Module):
-
-    def __init__(self, channels, vector_dim, rnn_hidden_size, rnn_num_layers):
-
-        # convolution layers arguments: input_channels, output_channels, filter_size, stride, padding.
-        super(CRNN, self).__init__()
-
         # CNN
-        self.conv1 = nn.Conv2d(1, channels, 3, stride=1, padding=1)    # output: (batch,96,192,32)
+        self.conv1 = nn.Conv2d(variables, channels, 3, stride=1, padding=1)    # output: (batch,96,192,32)
         self.conv1_bn = nn.BatchNorm2d(channels)
         self.conv2 = nn.Conv2d(channels, channels*2, 4, 2, 1)  #  (batch,48,96,64)
-        #self.conv2_drop = nn.Dropout2d(p=0.5)
         self.conv2_bn = nn.BatchNorm2d(channels*2)
         self.conv3 = nn.Conv2d(channels*2, channels*4, 4, 2, 1) # (batch,24,48,128)
-        #self.conv3_drop = nn.Dropout2d(p=0.5)
         self.conv3_bn = nn.BatchNorm2d(channels*4)
         self.conv4 = nn.Conv2d(channels*4, channels*8, 4, 2, 1) # (batch,12,24,256)
-        #self.conv4_drop = nn.Dropout2d(p=0.5)
         self.conv4_bn = nn.BatchNorm2d(channels*8)
         self.conv5 = nn.Conv2d(channels*8, channels*16, 4, 2, 1) # (batch,6,12,512)
         self.conv5_bn = nn.BatchNorm2d(channels*16)
         self.conv6 = nn.Conv2d(channels*16, channels*16, 4, 1, 0) # (batch,3,6,1024)
-        #self.conv6_drop = nn.Dropout2d(p=0.5)
-
         # Linear
         self.fc1 = nn.Linear(channels*16*3*9, vector_dim)
-        #self.fc1_drop = nn.Dropout(p=0.5)
+        # dropout regularization
+        self.dropout = dropout
+        if dropout != None:
+            self.conv2_drop = nn.Dropout2d(p=dropout)
+            self.conv3_drop = nn.Dropout2d(p=dropout)
+            self.conv4_drop = nn.Dropout2d(p=dropout)
+            self.conv5_drop = nn.Dropout2d(p=dropout)
+            self.conv6_drop = nn.Dropout2d(p=dropout)
+            self.fc1_drop = nn.Dropout(p=dropout)
 
-        # RNN
+    def forward(self, input):
+
+        if self.dropout == None:
+            x = F.relu(self.conv1_bn(self.conv1(input)))
+            x = F.relu(self.conv2_bn(self.conv2(x)))
+            x = F.relu(self.conv3_bn(self.conv3(x)))
+            x = F.relu(self.conv4_bn(self.conv4(x)))
+            x = F.relu(self.conv5_bn(self.conv5(x)))
+            x = F.relu(self.conv6(x))
+            # resize tensor and FC
+            x = x.view(x.size()[0], -1)
+            x = self.fc1(x)
+        else:
+            x = F.relu(self.conv1_bn(self.conv1(input)))
+            x = F.relu(self.conv2_bn(self.conv2(x)))
+            x = F.relu(self.conv3_bn(self.conv3(x)))
+            x = F.relu(self.conv4_bn(self.conv4_drop(self.conv4(x))))
+            x = F.relu(self.conv5_bn(self.conv5_drop(self.conv5(x))))
+            x = F.relu(self.conv6_drop(self.conv6(x)))
+            # resize tensor and FC
+            x = x.view(x.size()[0], -1)
+            x = self.fc1_drop(self.fc1(x))
+        return x
+        #eturn x.view(-1)
+
+
+class RNN(nn.Module):
+
+    def __init__(self, vector_dim, rnn_hidden_size, rnn_num_layers):
+        super(RNN, self).__init__()
         self.hidden_size = rnn_hidden_size
         self.num_layers = rnn_num_layers
         self.lstm = nn.LSTM(input_size=vector_dim, hidden_size=self.hidden_size, num_layers=self.num_layers, batch_first=True)
-
         # Linear
         self.fc2 = nn.Linear(self.hidden_size, 1)
 
-
     def forward(self, input):
 
-        batch_size, shape1, T, H, W = input.shape
-        input = input.reshape(batch_size*T, shape1, H, W)
-        #h0 = torch.zeros(self.num_layers, batch_size, self.hidden_size)
+        batch_size, T, dim = input.shape
         if torch.cuda.is_available():
             h0 = torch.randn(self.num_layers, batch_size, self.hidden_size).cuda()
             c0 = torch.zeros(self.num_layers, batch_size, self.hidden_size).cuda()
         else:
             h0 = torch.randn(self.num_layers, batch_size, self.hidden_size)
             c0 = torch.zeros(self.num_layers, batch_size, self.hidden_size)
+        out, _ = self.lstm(input, (h0, c0))
+        # Decode hidden state of last time step
+        out = self.fc2(out[:, -1, :])
+        return out
+
+class CRNN(nn.Module):
+
+    def __init__(self, variables, channels, vector_dim, rnn_hidden_size, rnn_num_layers, dropout=None):
+
+        # convolution layers arguments: input_channels, output_channels, filter_size, stride, padding.
+        super(CRNN, self).__init__()
 
         # CNN
-        x = F.relu(self.conv1_bn(self.conv1(input)))
-        x = F.relu(self.conv2_bn(self.conv2(x)))
-        x = F.relu(self.conv3_bn(self.conv3(x)))
-        x = F.relu(self.conv4_bn(self.conv4(x)))
-        x = F.relu(self.conv5_bn(self.conv5(x)))
-        x = F.relu(self.conv6(x))
+        self.conv1 = nn.Conv2d(variables, channels, 3, stride=1, padding=1)    # output: (batch,96,192,32)
+        self.conv1_bn = nn.BatchNorm2d(channels)
+        self.conv2 = nn.Conv2d(channels, channels*2, 4, 2, 1)  #  (batch,48,96,64)
+        self.conv2_bn = nn.BatchNorm2d(channels*2)
+        self.conv3 = nn.Conv2d(channels*2, channels*4, 4, 2, 1) # (batch,24,48,128)
+        self.conv3_bn = nn.BatchNorm2d(channels*4)
+        self.conv4 = nn.Conv2d(channels*4, channels*8, 4, 2, 1) # (batch,12,24,256)
+        self.conv4_bn = nn.BatchNorm2d(channels*8)
+        self.conv5 = nn.Conv2d(channels*8, channels*16, 4, 2, 1) # (batch,6,12,512)
+        self.conv5_bn = nn.BatchNorm2d(channels*16)
+        self.conv6 = nn.Conv2d(channels*16, channels*16, 4, 1, 0) # (batch,3,6,1024)
+        # Linear
+        self.fc1 = nn.Linear(channels*16*3*9, vector_dim)
+        # dropout regularization
+        self.dropout = dropout
+        if dropout != None:
+            self.conv2_drop = nn.Dropout2d(p=dropout)
+            self.conv3_drop = nn.Dropout2d(p=dropout)
+            self.conv4_drop = nn.Dropout2d(p=dropout)
+            self.conv5_drop = nn.Dropout2d(p=dropout)
+            self.conv6_drop = nn.Dropout2d(p=dropout)
+            self.fc1_drop = nn.Dropout(p=dropout)
 
-        # resize tensor and FC
-        x = x.view(x.size()[0], -1)
-        x = self.fc1(x)
+        self.hidden_size = rnn_hidden_size
+        self.num_layers = rnn_num_layers
+        self.lstm = nn.LSTM(input_size=vector_dim, hidden_size=self.hidden_size, num_layers=self.num_layers, batch_first=True)
+        # Linear
+        self.fc2 = nn.Linear(self.hidden_size, 1)
+
+
+    def forward(self, input):
+
+        # CNN
+        batch_size, shape1, T, H, W = input.shape
+        if torch.cuda.is_available():
+            h0 = torch.randn(self.num_layers, batch_size, self.hidden_size).cuda()
+            c0 = torch.zeros(self.num_layers, batch_size, self.hidden_size).cuda()
+        else:
+            h0 = torch.randn(self.num_layers, batch_size, self.hidden_size)
+            c0 = torch.zeros(self.num_layers, batch_size, self.hidden_size)
+        input = input.reshape(batch_size*T, shape1, H, W)
+        if self.dropout == None:
+            x = F.relu(self.conv1_bn(self.conv1(input)))
+            x = F.relu(self.conv2_bn(self.conv2(x)))
+            x = F.relu(self.conv3_bn(self.conv3(x)))
+            x = F.relu(self.conv4_bn(self.conv4(x)))
+            x = F.relu(self.conv5_bn(self.conv5(x)))
+            x = F.relu(self.conv6(x))
+            # resize tensor and FC
+            x = x.view(x.size()[0], -1)
+            x = self.fc1(x)
+        else:
+            x = F.relu(self.conv1_bn(self.conv1(input)))
+            x = F.relu(self.conv2_bn(self.conv2(x)))
+            x = F.relu(self.conv3_bn(self.conv3(x)))
+            x = F.relu(self.conv4_bn(self.conv4_drop(self.conv4(x))))
+            x = F.relu(self.conv5_bn(self.conv5_drop(self.conv5(x))))
+            x = F.relu(self.conv6_drop(self.conv6(x)))
+            # resize tensor and FC
+            x = x.view(x.size()[0], -1)
+            x = self.fc1_drop(self.fc1(x))
+
         x = x.reshape(batch_size, T, x.shape[-1])
 
-        # RNN
         out, _ = self.lstm(x, (h0, c0))
-
         # Decode hidden state of last time step
         out = self.fc2(out[:, -1, :])
 
-        return out.view(-1) #lstm1_out
+        return out.view(-1)
+
+'''
+class CRNN(nn.Module):
+
+    def __init__(self, variables, channels, vector_dim, rnn_hidden_size, rnn_num_layers, dropout=None):
+
+        super(CRNN, self).__init__()
+
+        # CNN
+        self.cnn = CNN(variables, channels, vector_dim, dropout)
+        # RNN
+        self.rnn = RNN(vector_dim, rnn_hidden_size, rnn_num_layers)
+
+    def forward(self, input):
+        # CNN
+        batch_size, shape1, T, H, W = input.shape
+        input = input.reshape(batch_size*T, shape1, H, W)
+        x = self.cnn(input)
+        x = x.reshape(batch_size, T, x.shape[-1])
+        # RNN
+        out = self.rnn(x)
+        return out.view(-1)
+
+'''
+'''
+For CRNN many-to-many we just need to change the output of self.fc2 to be 6 instead of 1,
+ and return out instead of out.view(-1)
+'''
+
+
+class CNN_Unet(nn.Module):
+
+    def __init__(self, variables, channels, vector_dim, dropout):
+
+        # convolution layers arguments: input_channels, output_channels, filter_size, stride, padding.
+        super(CNN_Unet, self).__init__()
+
+        # CNN
+        self.conv1 = nn.Conv2d(variables, 32, 3, stride=1, padding=1)    # output: (batch,96,192,64)
+        self.conv2 = nn.Conv2d(32, 32, 3, stride=1, padding=1)  #  (batch,96,192,64)
+        self.max1 = nn.MaxPool2d(kernel_size=2, stride=2)      #  (batch,48,96,64)
+
+        self.conv3 = nn.Conv2d(32, 64, 3, stride=1, padding=1)    # output: (batch,48,96,128)
+        self.conv4 = nn.Conv2d(64, 64, 3, stride=1, padding=1)  #  (batch,48,96,128)
+        self.max2 = nn.MaxPool2d(kernel_size=2, stride=2)         #  (batch,24,48,128)
+
+        self.conv5 = nn.Conv2d(64, 96, 3, stride=1, padding=1)    # output: (batch,24,48,256)
+        self.conv6 = nn.Conv2d(96, 96, 3, stride=1, padding=1)  #  (batch,24,48,256)
+        self.max3 = nn.MaxPool2d(kernel_size=2, stride=2)      #  (batch,12,24,256)
+
+        self.conv7 = nn.Conv2d(96, 128, 3, stride=1, padding=1)    # output: (batch,12,24,512)
+        self.conv8 = nn.Conv2d(128, 128, 3, stride=1, padding=1)  #  (batch,12,24,512)
+        self.max4 = nn.MaxPool2d(kernel_size=2, stride=2)         #  (batch,6, 12, 512)
+
+        # Linear
+        self.fc1 = nn.Linear(128*6*12, 500)
+
+
+    def forward(self, input):
+
+        # CNN
+        x = F.relu(self.conv1(input))
+        x = F.relu(self.conv2(x))
+        x = self.max1(x)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = self.max2(x)
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        x = self.max3(x)
+        x = F.relu(self.conv7(x))
+        x = F.relu(self.conv8(x))
+        x = self.max4(x)
+        # resize tensor and FC
+        x = x.view(x.size()[0], -1)
+        x = self.fc1(x)
+
+        return x
+
+class ResNetCRNN(nn.Module):
+
+    def __init__(self, variables, channels, vector_dim, rnn_hidden_size, rnn_num_layers, dropout=None):
+
+        super(ResNetCRNN, self).__init__()
+
+        # CNN
+        # ResNet uses inputs size 224x224
+        self.resnet = models.resnet50()
+        # RNN
+        self.resnet.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.resnet.fc = nn.Linear(1280, 500)
+        #self.conv = nn.Sequential(*list(self.resnet.children())[:-1])
+
+
+
+        self.rnn = RNN(vector_dim, rnn_hidden_size, rnn_num_layers)
+
+    def forward(self, input):
+        # CNN
+        batch_size, shape1, T, H, W = input.shape
+        input = input.reshape(batch_size*T, shape1, H, W)
+        x = self.resnet(input)
+        x = x.reshape(batch_size, T, x.shape[-1])
+        # RNN
+        out = self.rnn(x)
+        return out.view(-1)
+
 
 '''
 Here we can add the metrics we decide to use.
--- model/data_loader.py
+++ model/data_loader.py
@@ -1,9 +1,5 @@
-import random
 import os
-
-from PIL import Image
 from torch.utils.data import Dataset, DataLoader
-import torchvision.transforms as transforms
 import pandas as pd
 import numpy as np
 import torch
@@ -11,26 +7,36 @@ import torch
 
 class ClimateDataset(Dataset):
 
-    def __init__(self, data_dir, model_name):
+    def __init__(self, data_dir, model_name, split):
 
-        self.images_path = os.path.join(data_dir, "{}".format('small_data'))
-        self.labels_path = os.path.join(data_dir, "{}".format('small_data/labels.csv'))
-
+        self.images_path = os.path.join(data_dir, "{}".format('full_data_nst'))
+        self.labels_path = os.path.join(data_dir, "{}".format('full_data_nst/labels6m.csv'))
+        print('im using single channel dataloader')
+        print('labels_path: ', self.labels_path)
         self.filenames = sorted(os.listdir(self.images_path))
         self.filenames = [os.path.join(self.images_path, f) for f in self.filenames if f.endswith('.npy')]
         labels_df = pd.read_csv(self.labels_path)
-        self.labels = np.asarray(labels_df['tas'].tolist())
+
+        self.labels = np.asarray(labels_df['skt'].tolist())
+        if model_name == 'cnn_many':
+            # change labels to arrays of 6 months
+            # also need to change getitem function from labels to labels_many
+            self.labels_many = np.zeros((len(self.filenames), 6))
+            for i in range(len(self.filenames)):
+                self.labels_many[i,:] = self.labels[i:i+6]
+
         print('number of labels',len(self.labels))
         print('number of inputs', len(self.filenames))
 
         # load all inputs and assign correct dimensions depending on cnn or crnn
-        self.inputs = [np.load(self.filenames[idx]) for idx in range(len(self.filenames))]
-        if model_name == 'cnn':
-            self.inputs = [input.reshape(input.shape[2], input.shape[0], input.shape[1]) for input in self.inputs]
-        if model_name == 'crnn':
-            self.inputs = [np.expand_dims(input, axis = 0) for input in self.inputs]
-        self.inputs = [torch.FloatTensor(input) for input in self.inputs]
-
+        #self.inputs = [np.load(self.filenames[idx]) for idx in range(len(self.filenames))]
+        #print(self.inputs[0].shape)
+        #if model_name == 'cnn':
+         #   self.inputs = [input.reshape(input.shape[2], input.shape[0], input.shape[1]) for input in self.inputs]
+        #if model_name == 'crnn' or model_name == 'crnn_many' or model_name == 'crnn_unet':
+         #   self.inputs = [np.expand_dims(input, axis = 0) for input in self.inputs]
+        #self.inputs = [torch.FloatTensor(input) for input in self.inputs]
+        #print(self.inputs[0].shape)
 
     def __len__(self):
         # return size of dataset
@@ -38,7 +44,9 @@ class ClimateDataset(Dataset):
 
     def __getitem__(self, idx):
 
-        return self.inputs[idx], self.labels[idx]  # return both as tensors
+        input = np.load(self.filenames[idx])
+        input = torch.FloatTensor(np.expand_dims(input, axis = 0))
+        return input, self.labels[idx]  # return both as tensors
 
 
 def fetch_dataloader(types, data_dir, batch_size, model_name):
@@ -52,12 +60,15 @@ def fetch_dataloader(types, data_dir, batch_size, model_name):
 
             if split == 'train':
                 print('loading training set')
-                dl = DataLoader(ClimateDataset(path, model_name), batch_size=batch_size, shuffle=True)
+                dl = DataLoader(ClimateDataset(path, model_name, split), batch_size=batch_size, num_workers=1, shuffle=False)
 
-            else:
+            elif split == 'val':
                 print('loading validation set')
-                dl = DataLoader(ClimateDataset(path, model_name), batch_size=batch_size, shuffle=False)
+                dl = DataLoader(ClimateDataset(path, model_name, split), batch_size=batch_size, num_workers=1, shuffle=False)
 
+            elif split == 'test':
+                print('loading test set')
+                dl = DataLoader(ClimateDataset(path, model_name, split), batch_size=batch_size, shuffle=False)
 
             dataloaders[split] = dl
 
